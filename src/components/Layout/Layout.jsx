04df311aeb30aa2f369d6ca297dc1c462c8552b4
DavidHakak <davidhakak19@gmail.com>
2022-11-24 17:36:57 +0200

    update style css

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main"
+import "./Layout.css"
 
 function Layout() {
   return (
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./Table.css";
 
 function Table(props) {
+  let counter = 0;
   function createLinesInTable(classes) {
+    counter++;
     return (
       <tr>
+        <td>{counter}</td>
         <td>{classes.teacherName}</td>
         <td>{classes.theProgrammingLanguage}</td>
         <td>{classes.date}</td>
         <td>{classes.theLessonNumber}</td>
-        <td>{classes.theContentOfTheLesson}</td>
+        <td>
+          <p>{classes.theContentOfTheLesson}</p>
+        </td>
         <td>
           <a href={classes.linkToTheLesson} target={"_blank"} rel="noreferrer">
             <button>לצפייה בשיעור</button>
@@ -21,45 +26,48 @@ function Table(props) {
   }
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>שם המורה</th>
-          <th>שם השפה</th>
-          <th>תאריך</th>
-          <th>מס׳ שיעור</th>
-          <th>תוכן השיעור</th>
-          <th>קישור לשיעור</th>
-          <th>מצגות וכו׳</th>
-        </tr>
-      </thead>
+    <div className="tablaContainer">
+      <table className="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>שם המורה</th>
+            <th>שם השפה</th>
+            <th>תאריך</th>
+            <th>מס׳ שיעור</th>
+            <th>תוכן השיעור</th>
+            <th>קישור לשיעור</th>
+            <th>מצגות וכו׳</th>
+          </tr>
+        </thead>
 
-      <tbody>
-        {props.theListOfClasses.map((classes) => {
-          if (
-            props.teacher === "allTeachers" &&
-            props.language === "allLanguage"
-          ) {
-            return createLinesInTable(classes);
-          } else if (
-            props.teacher === "allTeachers" &&
-            classes.theProgrammingLanguage === props.language
-          ) {
-            return createLinesInTable(classes);
-          } else if (
-            classes.teacherName === props.teacher &&
-            props.language === "allLanguage"
-          ) {
-            return createLinesInTable(classes);
-          } else if (
-            classes.teacherName === props.teacher &&
-            classes.theProgrammingLanguage === props.language
-          ) {
-            return createLinesInTable(classes);
-          }
-        })}
-      </tbody>
-    </table>
+        <tbody>
+          {props.theListOfClasses.map((classes) => {
+            if (
+              props.teacher === "allTeachers" &&
+              props.language === "allLanguage"
+            ) {
+              return createLinesInTable(classes);
+            } else if (
+              props.teacher === "allTeachers" &&
+              classes.theProgrammingLanguage === props.language
+            ) {
+              return createLinesInTable(classes);
+            } else if (
+              classes.teacherName === props.teacher &&
+              props.language === "allLanguage"
+            ) {
+              return createLinesInTable(classes);
+            } else if (
+              classes.teacherName === props.teacher &&
+              classes.theProgrammingLanguage === props.language
+            ) {
+              return createLinesInTable(classes);
+            }
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
